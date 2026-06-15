c149aba4d2e6e379671371fc1f8fe383f1ada8a6
Adithya Jannu <adijannu09@gmail.com>
2026-06-15 23:13:07 +0530

    Day 1 - Project setup and user profile complete

---
+++ main.py
@@ -0,0 +1,23 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city name: ")
+question = input("What do you want to learn: ")
+
+next_year_age = age + 1
+years_to_60 = 60 - age
+
+print("--------------------")
+print("Welcome to AI Assistant!")
+print("--------------------")
+
+print(f"Name     : {name}")
+print(f"Age      : {age}")
+print(f"City     : {city}")
+print(f"Question : {question}")
+
+print("--------------------")
+
+print(f"Next year age : {next_year_age}")
+print(f"Years to 60   : {years_to_60}")
+
+print("--------------------")
