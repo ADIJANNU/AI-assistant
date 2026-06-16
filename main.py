ac55ff9236eb0187644c9072a198dae0608be2e1
Adithya Jannu <adijannu09@gmail.com>
2026-06-16 20:31:58 +0530

    Day 2 - String methods and input cleaning added

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
-name = input("Enter your name: ")
-age = int(input("Enter your age: "))
-city = input("Enter your city name: ")
-question = input("What do you want to learn: ")
 
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+print("   Welcome to AI Assistant")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+
+
+name = input("Enter your name: ").strip().title()
+age = int(input("Enter your age: ").strip())
+city = input("Enter your city name: ").strip().title()
+goal = input("What do you want to learn: ").strip().lower()
+
+firstname = name.split()[0]
 next_year_age = age + 1
 years_to_60 = 60 - age
+goal_words = len(goal.split())
 
-print("--------------------")
-print("Welcome to AI Assistant!")
-print("--------------------")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+print(f"Hello {firstname}!")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+print(f"{'Name': <10} : {name}")
+print(f"{'Age': <10}  : {age}")
+print(f"{'City': <10} : {city}")
+print(f"{'Question' : <10} : {goal.title()}")
 
-print(f"Name     : {name}")
-print(f"Age      : {age}")
-print(f"City     : {city}")
-print(f"Question : {question}")
-
-print("--------------------")
-
-print(f"Next year age : {next_year_age}")
-print(f"Years to 60   : {years_to_60}")
-
-print("--------------------")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
+print(f"{'Next year age' : <10} : {next_year_age}")
+print(f"{'Years to 60': <10}  : {years_to_60}")
+print(f"{'Goal words': <10}: {goal_words} ")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━")
