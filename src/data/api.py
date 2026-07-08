4dae6a0890ca2a7c421dcc8ab374072fcd9b932b
Adithya Jannu <adijannu09@gmail.com>
2026-07-08 23:25:45 +0530

    Day 15 - api added

--- src/data/api.py
+++ src/data/api.py
@@ -13,3 +13,23 @@ def get_advice():
   
 result = get_advice()
 print(result)
+
+
+def get_country_info(code):
+  response = requests.get(f"https://countries.dev/alpha/{code}")
+  data = response.json()
+  # print(data)
+  # print(response.text)
+  print(response.status_code)
+
+  if response.status_code == 200:
+    name = data["name"]
+    capital = data["capital"]
+    population = data["population"]
+
+    return f"{name}'s capital is {capital} with a population of {population}"
+  else:
+    print("Couldn't fetch advice right now")
+
+result = get_country_info("in")
+print(result)
